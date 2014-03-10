cf6ef97fe5b326eedc7e394d08bd1bad915a6cbe
Alessandro Giorgetti <guardian@nablasoft.com>
2014-03-10 14:14:23 +0100

    more slides & demo project skeleton

---
+++ demo/app/app.js
@@ -0,0 +1,7 @@
+﻿(function () {
+    angular.module('myApp', [])
+        .controller('shell', ['$scope', function ($scope) {
+            $scope.title = "IoC / DI";
+            $scope.mainContent = "main content";
+        }]);
+})();
